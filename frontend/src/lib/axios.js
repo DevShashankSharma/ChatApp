30be5bc0bdc56bbb5c1e27430b34886875dbbe76
DevShashankSharma <editorshashank429@gmail.com>
2025-12-02 08:32:57 +0530

    cors issue

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export const axiosInstance = axios.create({
     baseURL: "https://chat-app-ipbs.vercel.app/",  //! base url for the api
-    // withCredentials: true,  //! send cookies when cross-domain requests
+    withCredentials: true,  //! send cookies when cross-domain requests
 }); 
