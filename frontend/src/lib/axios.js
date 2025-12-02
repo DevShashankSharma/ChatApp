fd4ebb8c8791a377f1aad5a58589cd8452b64e53
DevShashankSharma <editorshashank429@gmail.com>
2025-12-02 07:57:31 +0530

    database changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: "https://chat-app-vert-pi.vercel.app/",  //! base url for the api
-    withCredentials: true,  //! send cookies when cross-domain requests
+    baseURL: "https://chat-app-ipbs.vercel.app/",  //! base url for the api
+    // withCredentials: true,  //! send cookies when cross-domain requests
 }); 
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -16,7 +16,7 @@ export const useAuthStore = create((set, get) => ({
 
     checkAuth: async () => {
         try {
-            const res = await axiosInstance.get('https://chat-app-ipbs.vercel.app/auth/check');
+            const res = await axiosInstance.get('api/auth/check');
 
             set({ authUser: res.data })
 
@@ -32,7 +32,7 @@ export const useAuthStore = create((set, get) => ({
     signup: async (data) => {
         try {
             set({ isSigningUp: true })
-            const res = await axiosInstance.post('https://chat-app-ipbs.vercel.app/auth/signup', data);
+            const res = await axiosInstance.post('api/auth/signup', data);
             set({ authUser: res.data })
             toast.success("Account created successfully");
 
@@ -46,7 +46,7 @@ export const useAuthStore = create((set, get) => ({
 
     logout: async () => {
         try {
-            await axiosInstance.post('https://chat-app-ipbs.vercel.app/auth/logout');
+            await axiosInstance.post('api/auth/logout');
             set({ authUser: null });
             toast.success("Logged out successfully");
 
@@ -60,7 +60,7 @@ export const useAuthStore = create((set, get) => ({
     login: async (data) => {
         try {
             set({ isLoggingIn: true });
-            const res = await axiosInstance.post('https://chat-app-ipbs.vercel.app/auth/login', data);
+            const res = await axiosInstance.post('api/auth/login', data);
             set({ authUser: res.data });
             toast.success("Logged in successfully");
 
@@ -75,7 +75,7 @@ export const useAuthStore = create((set, get) => ({
     updateProfile: async (data) => {
         try {
             set({ isUpdatingProfile: true });
-            const res = await axiosInstance.put('https://chat-app-ipbs.vercel.app/auth/update-profile', data);
+            const res = await axiosInstance.put('api/auth/update-profile', data);
             set({ authUser: res.data });
             toast.success("Profile updated successfully");
         } catch (error) {
