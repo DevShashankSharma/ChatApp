7426d02db631d3e4bc0b43cd5f80c90270373b3c
DevShashankSharma <editorshashank429@gmail.com>
2025-12-02 06:00:02 +0530

    error correction

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,9 +16,9 @@ const PORT = process.env.PORT;
 app.use(express.json({ limit: "50mb" }));  //! set limit to 50mb so that large files can be uploaded
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5173",  
     credentials: true
-}));
+})); 
 
 
 app.use("/api/auth", authRoutes);
