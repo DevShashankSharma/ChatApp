e7b5abbc65e165ed42566d7e5d46f95aae2b6d76
DevShashankSharma <editorshashank429@gmail.com>
2026-02-10 13:22:57 +0530

    updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,15 +8,15 @@ import cors from 'cors';
 import { app, server, io } from './lib/socket.js';
 
 // const app = express();
-dotenv.config();
-const PORT = process.env.PORT;
+dotenv.config(); 
+const PORT = process.env.PORT;  
 
 
 
 app.use(express.json({ limit: "50mb" }));  //! set limit to 50mb so that large files can be uploaded
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5173", 
     credentials: true
 }));
 
